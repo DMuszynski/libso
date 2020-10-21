d9809764830334731de0a4c8ed36ad427856a903
DAMIAN_MUSZYNSKI <d.muszynski112@gmail.com>
2020-10-22 01:37:03 +0200

    first commit

---
+++ libso-api/src/main/java/pl/dmuszynski/libso/LibsoApiApplication.java
@@ -0,0 +1,13 @@
+package pl.dmuszynski.libso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibsoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibsoApiApplication.class, args);
+	}
+
+}
---
+++ libso-api/src/test/java/pl/dmuszynski/libso/LibsoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dmuszynski.libso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibsoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
